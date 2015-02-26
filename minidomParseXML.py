329139eeeab771afa273e13a1f94383f1b80d2d5
TomTheToad <tomthetoad951@gmail.com>
2015-02-26 12:27:10 -0700

    Fixed loop problem which did not properly update the alt variable

--- minidomParseXML.py
+++ minidomParseXML.py
@@ -5,11 +5,13 @@ xmldoc = minidom.parse("422.gpx")
 trkpt = xmldoc.getElementsByTagName("trkpt")
 ele = xmldoc.getElementsByTagName("ele")
  
+count = 0
+
 for points in trkpt:
     lat = points.attributes['lat'].value
     lon = points.attributes['lon'].value
-    for items in ele:
-        alt = items.firstChild.nodeValue
+    alt = ele[count].firstChild.nodeValue
+    count += 1
     print("lat:" + str(lat) + " lon:" + str(lon) + " alt:" + str(alt) + "\n")
     
 
